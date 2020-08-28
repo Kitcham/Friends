1ecbcb745c66b4b3444dce678af3987dafcb0f54
Kitcham <kitcham@outlook.com>
2020-08-28 13:36:18 +0800

    test(FTP): test FTP config avoid cache
    Test FTP config avoid cache.

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import json
 import yaml
+import sys
 
 
 def _loadYaml(yamlfile):
@@ -8,12 +9,15 @@ def _loadYaml(yamlfile):
     return linksList
 
 
-def _renderJson(yamlList, jsonFile):
+def _renderJson(yamlList, jsonFile, localFile):
     with open(jsonFile, 'w', encoding='utf8') as linksJsonData:
         linksJsonData.write(json.dumps(yamlList, ensure_ascii=False))
         linksJsonData.close()
+    with open(localFile, 'w', encoding='utf8') as linksJsonData:
+        linksJsonData.write(json.dumps(yamlList, ensure_ascii=False))
+        linksJsonData.close()
 
-
+localTarget = sys.argv[1]
 yamlOrigin = "./src/links.yml"
 jsonTarget = "./links/links.json"
-_renderJson(_loadYaml(yamlOrigin), jsonTarget)
+_renderJson(_loadYaml(yamlOrigin), jsonTarget, localTarget)
